e6981742de31ffb117ca41de0c33cabfb7491ca4
McKenzie Custer <custermr@mail.uc.edu>
2020-11-10 19:12:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+//Worked on this with Jacob Leusch
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*; 
+
+class Main {
+  public static void main(String args[]){
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        new GuessingGame();
+      }
+    });
+  }
+}
